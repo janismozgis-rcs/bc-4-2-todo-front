adfbb27bcd34c9322c332b9bbadab09a1e6bfc74
janismozgis-rcs <janismozgis+rcs@gmail.com>
2022-03-07 20:02:58 +0200

    Lesson 28 prep

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { getTasks } from "./API/tasks";
+import CreateNewTask from "./Components/CreateNewTask";
+import TasksList from "./Components/TasksList";
 
 function App() {
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const tasksFromBackend = getTasks()
+    setTasks(tasksFromBackend)
+    
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My tasks:</h1>
+      <TasksList tasks={tasks} />
+      <h3>Create a new task</h3>
+      <CreateNewTask />
     </div>
   );
 }
